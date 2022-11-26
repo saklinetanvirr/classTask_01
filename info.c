0f3547cf03e9d8e1d129738817c5a11782fd5d43
saklinetanvirr <103268297+saklinetanvirr@users.noreply.github.com>
2022-11-26 19:16:48 +0600

    Add files via upload

---
+++ info.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("\tName: MD. Sakline Hossen\n\n\tUniversity: Premier University Chittagong\n\n\tDepartment: Computer Science and Engineering\n\n\tID: 02222200051053\n\n");
+
+    return 0;
+}
